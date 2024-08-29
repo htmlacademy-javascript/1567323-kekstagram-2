d14d794221a88015ea80b0639da0e903a9525493
Sergey Proskuryakov <psvrus@yandex.ru>
2024-08-29 21:29:12 +0700

    Отрисуй меня полностью

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -3,25 +3,25 @@ import { getPhotosArray } from './create-photo.js';
 const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
 const containerPicture = document.querySelector('.pictures');
 
-const getPicture = getPhotosArray();
+const getPictures = getPhotosArray();
 
-const fragment = document.createDocumentFragment();
+const thumbnailsFragment = document.createDocumentFragment();
 
-getPicture.forEach((photo) => {
+getPictures.forEach((photo) => {
 
-  const photoClone = templatePicture.cloneNode(true);
-  const image = photoClone.querySelector('.picture__img');
+  const thumbnailPhoto = templatePicture.cloneNode(true);
+  const image = thumbnailPhoto.querySelector('.picture__img');
 
   image.src = photo.url;
   image.alt = photo.description;
 
-  photoClone.querySelector('.picture__comments').textContent = photo.comments.length;
-  photoClone.querySelector('.picture__likes').textContent = photo.likes;
+  thumbnailPhoto.querySelector('.picture__comments').textContent = photo.comments.length;
+  thumbnailPhoto.querySelector('.picture__likes').textContent = photo.likes;
 
-  fragment.appendChild(photoClone);
+  thumbnailsFragment.appendChild(thumbnailPhoto);
 });
 
-containerPicture.appendChild(fragment);
+containerPicture.appendChild(thumbnailsFragment);
 
 /*
 2. На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:

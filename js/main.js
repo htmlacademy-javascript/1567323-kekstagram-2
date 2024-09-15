37f3790f68adcd636d2532349a31b4244f017055
Sergey Proskuryakov <psvrus@yandex.ru>
2024-09-15 20:01:37 +0700

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-import {containerPictures, renderPictures} from './thumbnails.js';
-import {openPhotoModal} from './photo-modal.js';
+import { containerPictures, renderPictures } from './thumbnails.js';
+import { openPhotoModal } from './photo-modal.js';
+import { openPhotoEditor } from './photo-upload-form.js';
+
 
 renderPictures();
+
 
 containerPictures.addEventListener('click', (evt) => {
   const currentPhoto = evt.target.closest('.picture');
@@ -10,3 +13,5 @@ containerPictures.addEventListener('click', (evt) => {
     openPhotoModal(currentPhoto.dataset.photoId);
   }
 });
+
+openPhotoEditor();
